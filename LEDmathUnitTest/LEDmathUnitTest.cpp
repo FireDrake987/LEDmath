8ce1942613ddc7d48262de85776cab1876e59ac4
FireDrake987 <danielmlouk@gmail.com>
2025-11-27 07:52:32 -0600

    Updated Git include

---
+++ LEDmathUnitTest/LEDmathUnitTest.cpp
@@ -0,0 +1,16 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace LEDmathUnitTest
+{
+	TEST_CLASS(LEDmathUnitTest)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+}
